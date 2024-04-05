fb3d7a1512edd507c1cbfd5bf25de60bdd4d69a3
haweeee <109846781+haweeee@users.noreply.github.com>
2024-04-05 17:27:53 +0900

    프로젝트 생성

---
+++ src/main/java/TubeSlice/tubeSlice/TubeSliceApplication.java
@@ -0,0 +1,13 @@
+package TubeSlice.tubeSlice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubeSliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubeSliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TubeSlice/tubeSlice/TubeSliceApplicationTests.java
@@ -0,0 +1,13 @@
+package TubeSlice.tubeSlice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubeSliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
