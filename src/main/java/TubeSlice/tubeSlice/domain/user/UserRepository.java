cf0c5abea5bef70f400c998df70d3e77494223ad
hsuush <dlhyes703@naver.com>
2024-04-24 23:29:24 +0900

    feat : 팔로우 하기 및 취소하기 구현

--- src/main/java/TubeSlice/tubeSlice/domain/user/UserRepository.java
+++ src/main/java/TubeSlice/tubeSlice/domain/user/UserRepository.java
@@ -3,9 +3,13 @@ package TubeSlice.tubeSlice.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByLoginId(String loginId);
+
+    Optional<User> findById(long userId);
 
 }
