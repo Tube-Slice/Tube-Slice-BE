14306daaee8215e399d850b206b0e2c237806221
hsuush <dlhyes703@naver.com>
2024-04-25 00:29:28 +0900

    chore : 잘못된 브랜치

--- src/main/java/TubeSlice/tubeSlice/domain/keyword/KeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package TubeSlice.tubeSlice.domain.keyword;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Keyword findByName(String name);
-
-    boolean existsByName(String name);
-}
