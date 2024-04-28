dc378762ca04c22c4cdb7fca3d2522a6c79c2c92
hsuush <dlhyes703@naver.com>
2024-04-29 01:26:13 +0900

    shore : swagger 세팅 및 충돌 해결

--- src/main/java/TubeSlice/tubeSlice/domain/user/UserController.java
+++ src/main/java/TubeSlice/tubeSlice/domain/user/UserController.java
@@ -3,6 +3,8 @@ package TubeSlice.tubeSlice.domain.user;
 import TubeSlice.tubeSlice.domain.post.dto.PostResponseDto;
 import TubeSlice.tubeSlice.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import TubeSlice.tubeSlice.domain.keyword.dto.response.KeywordResponseDto;
 import TubeSlice.tubeSlice.global.response.ApiResponse;
@@ -34,17 +36,13 @@ public class UserController {
     }
 
 
-    @GetMapping("/me/keywords")
-    public ApiResponse<List<KeywordResponseDto.KeywordResultDto>> getMyKeywordList(){
-        User user = userService.findUser(1L);
-
-        return ApiResponse.onSuccess(userService.getPostList(user));
-    }
-
     @GetMapping("/{userId}/posts")
     @Operation(summary = "특정유저의 게시글 목록 가져오기 API",description = "특정유저가 작성한 게시글 목록 가져오기, PostInfoDto의 list를 반환")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "특정 유저의 id"),
     })
     public ApiResponse<List<PostResponseDto.PostInfoDto>> getUserPostList(@PathVariable(name = "userId")Long userId){
         User user = userService.findUser(userId);
@@ -53,6 +51,10 @@ public class UserController {
     }
 
     @GetMapping("/me/keywords")
+    @Operation(summary = "나의 키워드 목록 가져오기 API",description = "내가 작성한 키워드 목록 가져오기, KeywordResponseDto list를 반환")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+    })
     public ApiResponse<List<KeywordResponseDto.KeywordResultDto>> getMyKeywordList(){
         User user = userService.findUser(1L);
 
@@ -60,6 +62,13 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/keywords")
+    @Operation(summary = "특정유저의 키워드 목록 가져오기 API",description = "특정유저가 작성한 키워드 목록 가져오기, KeywordResponseDto list를 반환")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "특정 유저의 id"),
+    })
     public ApiResponse<List<KeywordResponseDto.KeywordResultDto>> getUserKeywordList(@PathVariable(name = "userId")Long userId){
         User user = userService.findUser(userId);
 
--- src/main/java/TubeSlice/tubeSlice/domain/user/UserService.java
+++ src/main/java/TubeSlice/tubeSlice/domain/user/UserService.java
@@ -4,9 +4,6 @@ package TubeSlice.tubeSlice.domain.user;
 import TubeSlice.tubeSlice.domain.keyword.KeywordConverter;
 import TubeSlice.tubeSlice.domain.keyword.dto.response.KeywordResponseDto;
 import TubeSlice.tubeSlice.domain.post.Post;
-import TubeSlice.tubeSlice.global.response.code.resultCode.ErrorStatus;
-import TubeSlice.tubeSlice.global.response.exception.handler.UserHandler;
-import TubeSlice.tubeSlice.domain.post.Post;
 import TubeSlice.tubeSlice.domain.post.PostConverter;
 import TubeSlice.tubeSlice.domain.post.dto.PostResponseDto;
 import TubeSlice.tubeSlice.global.response.code.resultCode.ErrorStatus;
@@ -15,9 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 import java.util.Collections;
 import java.util.List;
@@ -41,10 +35,11 @@ public class UserService {
         return PostConverter.toPostInfoDtoList(postList);
     }
 
-        Collections.reverse(postList);
+    public List<KeywordResponseDto.KeywordResultDto> getUserKeywordList(User user){
+        List<Post> postList = user.getPostList();
 
-
-
+        return KeywordConverter.toKeywordResultDtoList(postList);
+    }
 
 }
 
