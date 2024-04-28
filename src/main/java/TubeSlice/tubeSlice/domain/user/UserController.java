7f5d5de8f5e185ba995f5961c9d73bb8f6de6467
hsuush <dlhyes703@naver.com>
2024-04-28 16:10:36 +0900

    feat : 유저의 키워드 목록 가져오기

--- src/main/java/TubeSlice/tubeSlice/domain/user/UserController.java
+++ src/main/java/TubeSlice/tubeSlice/domain/user/UserController.java
@@ -1,15 +1,32 @@
 package TubeSlice.tubeSlice.domain.user;
 
+import TubeSlice.tubeSlice.domain.keyword.dto.response.KeywordResponseDto;
+import TubeSlice.tubeSlice.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/users")
 public class UserController {
+    private final UserService userService;
 
+    @GetMapping("/me/keywords")
+    public ApiResponse<List<KeywordResponseDto.KeywordResultDto>> getMyKeywordList(){
+        User user = userService.findUser(1L);
 
+        return ApiResponse.onSuccess(userService.getUserKeywordList(user));
+    }
+
+    @GetMapping("/{userId}/keywords")
+    public ApiResponse<List<KeywordResponseDto.KeywordResultDto>> getUserKeywordList(@PathVariable(name = "userId")Long userId){
+        User user = userService.findUser(userId);
+
+        return ApiResponse.onSuccess(userService.getUserKeywordList(user));
+    }
 }
--- src/main/java/TubeSlice/tubeSlice/domain/user/UserService.java
+++ src/main/java/TubeSlice/tubeSlice/domain/user/UserService.java
@@ -1,17 +1,34 @@
 package TubeSlice.tubeSlice.domain.user;
 
 
+import TubeSlice.tubeSlice.domain.keyword.KeywordConverter;
+import TubeSlice.tubeSlice.domain.keyword.dto.response.KeywordResponseDto;
+import TubeSlice.tubeSlice.domain.post.Post;
+import TubeSlice.tubeSlice.global.response.code.resultCode.ErrorStatus;
+import TubeSlice.tubeSlice.global.response.exception.handler.UserHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
+    private final UserRepository userRepository;
 
+    public User findUser(Long userId){
+        return userRepository.findById(userId).orElseThrow(()-> new UserHandler(ErrorStatus.USER_NOT_FOUND));
+    }
 
+    public List<KeywordResponseDto.KeywordResultDto> getUserKeywordList(User user){
+        List<Post> postList = user.getPostList();
+
+        return KeywordConverter.toKeywordResultDtoList(postList);
+    }
 }
 
