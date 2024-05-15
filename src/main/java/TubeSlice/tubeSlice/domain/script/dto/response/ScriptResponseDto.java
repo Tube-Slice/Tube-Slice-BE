eb7252220999e1c4cff18e6e1e0acc4c677b28f5
haweeee <kcu3523@gmail.com>
2024-05-15 18:14:50 +0900

    refactor : textResponseDto 옮김

--- src/main/java/TubeSlice/tubeSlice/domain/script/dto/response/ScriptResponseDto.java
+++ src/main/java/TubeSlice/tubeSlice/domain/script/dto/response/ScriptResponseDto.java
@@ -1,6 +1,7 @@
 package TubeSlice.tubeSlice.domain.script.dto.response;
 
 import TubeSlice.tubeSlice.domain.scriptKeyword.ScriptKeyword;
+import TubeSlice.tubeSlice.domain.text.dto.response.TextResponseDto;
 import TubeSlice.tubeSlice.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,15 +22,6 @@ public class ScriptResponseDto {
     private List<ScriptKeyword> keywords;
     private List<SubtitleResponseDto> subtitles;
 
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class TextResponseDto {
-
-        private Double timeline;
-        private String scripts;
-    }
 
     @Getter
     @Builder
