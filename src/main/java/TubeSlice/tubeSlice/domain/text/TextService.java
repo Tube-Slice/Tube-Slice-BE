a7fa0341d191648eecbdfaf65b7226ef76e110d1
haweeee <kcu3523@gmail.com>
2024-05-20 16:58:40 +0900

    refactor: 유튜브 링크에서 mp3 추출 추가 및 요약하기 질문 수정

--- src/main/java/TubeSlice/tubeSlice/domain/text/TextService.java
+++ src/main/java/TubeSlice/tubeSlice/domain/text/TextService.java
@@ -61,7 +61,8 @@ public class TextService {
 
 
     @Transactional
-    public Object videoToScript(String filePath) {
+    public List<Map.Entry<Double, String>> videoToScript(String youtubeUrl) {
+        String filePath = getAudioFileFromYoutubeUrl(youtubeUrl);
         String objectStorageDataKey = uploadFile(filePath);
 
         HttpHeaders headers = new HttpHeaders();
@@ -95,8 +96,8 @@ public class TextService {
         if (findScript == null) {
 
             script = Script.builder()
-                    .videoTitle(null)
-                    .videoUrl(objectStorageDataKey)
+                    .videoTitle(objectStorageDataKey)
+                    .videoUrl(youtubeUrl)
                     .scriptTitle(fileName)
                     .build();
 
@@ -109,9 +110,9 @@ public class TextService {
     }
 
     public Object summarize(TextRequestDto.SummaryRequestDto summaryRequestDto){
-        String requestMessage1 = "내 질문에 대한 응답 json 형식으로 출력해줘.";
-        String requestMessage2 = "\"" + summaryRequestDto.getScript() + "\" 이 스크립트에서 핵심 내용" + summaryRequestDto.getRow() + "문장으로 요약해서 구분해서 보여줘.\n" +
-                "출력 형식:  \"'1':'요약내용'\"";
+        String requestMessage1 = "내 질문에 대한 응답을 json 형식으로 출력해줘.";
+        String requestMessage2 = "\"" + summaryRequestDto.getScript() + "\"\n이 스크립트에서 핵심 내용을 요약해서 이해하기 쉽게"+ summaryRequestDto.getRow() + "문장으로 설명해줘.\n" +
+                "출력 형식은 \"'1':'설명내용'\" 에 맞게 답해줘.";
 
         List<TextRequestDto.GptRequest.Messages> messages = new ArrayList<>();
         messages.add(new TextRequestDto.GptRequest.Messages("system",requestMessage1));
@@ -125,7 +126,7 @@ public class TextService {
 
         String content = gptResponse.getChoices().get(0).getMessage().getContent();
 
-        return trimResult(content);
+        return trimSummary(content);
     }
 
     public String uploadFile(String filePath) {
@@ -226,13 +227,25 @@ public class TextService {
         for (Map.Entry<Double, String> e : scripts){
             totalScriptsWithTimeline += e.getKey() + ":" + e.getValue() + "\n";
         }
+        log.info("totalScriptWithTimeLine: {}", totalScriptsWithTimeline);
 
         return totalScriptsWithTimeline;
     }
 
+    public String getTotalScript(List<Map.Entry<Double, String>> scripts){
+        String totalScript = "";
+
+        for (Map.Entry<Double, String> e : scripts){
+            totalScript += e.getValue() + "\n";
+        }
+        log.info("totalScript: {}", totalScript);
+
+        return totalScript;
+    }
+
     @Transactional
     public String trimText(String script){
-        String requestMessage1 = "내 질문에 대한 응답 json 형식으로 출력해줘.";
+        String requestMessage1 = "내 질문에 대한 응답을 json 형식으로 출력해줘.";
         String requestMessage2 = "\"" + script + "\" \n위 전체 스크립트를 읽어보고 중요한 핵심적인 내용에 해당하는 부분만 소제목 지어서 해당하는 시간과 소제목을 출력해줘. \n" +
                 "출력 형식: \"'시간':'소제목'\"";
 
@@ -250,15 +263,14 @@ public class TextService {
     }
 
     private HashMap<Double,String> trimResult(String jsonResult){
-
-        log.info("소제목 추출 결과: {}", jsonResult);
+        log.info("요약 내용: {}", jsonResult);
         HashMap<Double, String> result = new HashMap<>();
 
-        jsonResult = jsonResult.replaceAll("\n", "").trim();
+        //jsonResult = jsonResult.replaceAll("\n", "").trim();
         jsonResult = jsonResult.replace("{", "").trim();
         jsonResult = jsonResult.replaceAll("}", "").trim();
 
-        List<String> lines = List.of(jsonResult.split(","));
+        List<String> lines = List.of(jsonResult.split("\n"));
 
         for (String line : lines) {
 
@@ -276,33 +288,77 @@ public class TextService {
         return result;
     }
 
+    private HashMap<String,String> trimSummary(String jsonResult){
+        log.info("요약 내용: {}", jsonResult);
+        HashMap<String, String> result = new HashMap<>();
+
+        //jsonResult = jsonResult.replaceAll("\n", "").trim();
+        jsonResult = jsonResult.replace("{", "").trim();
+        jsonResult = jsonResult.replaceAll("}", "").trim();
+
+        List<String> lines = List.of(jsonResult.split("\n"));
+
+        for (String line : lines) {
+
+            List<String> parts = List.of(line.split(":"));
+
+            String value = parts.get(1).replaceAll("\"", "");
+
+            String idx = parts.get(0).replaceAll("\"","");
+
+            result.put(idx,value);
+
+            log.info("{} : {}", idx, value);
+        }
+
+        return result;
+    }
 
 
     public String getAudioFileFromYoutubeUrl(String youtubeUrl){
+        String filename = "mp3 파일 가져오기 실패.";
+
+        // yt-dlp.exe의 절대 경로 사용
+        //리눅스에서는 \말고 /사용.
+        //String ytDlpPath = "yt-dlp";
+        String ytDlpPath = "D:\\youtube-dl\\ffmpeg-7.0-essentials_build\\bin\\yt-dlp.exe";
+        //String downloadDir = "yt-dlp/mp3/%(title)s.%(ext)s";
+        String downloadDir = "D:\\youtube-dl\\%(title)s.%(ext)s";
 
         try {
             // Command to download video
-            String downloadCommand = "youtube-dl -f bestaudio -x --audio-format mp3 -o 'temp.%(ext)s' " + youtubeUrl;
-            // Command to extract audio
-            String extractCommand = "ffmpeg -i temp.mp3 -vn -acodec copy output.mp3";
+            String downloadCommand = String.format("%s -x --audio-format mp3 -o \"%s\" %s", ytDlpPath, downloadDir, youtubeUrl);
 
-            // Download video
+            // Command to print filename
+            String printCommand = String.format("%s --print filename -o \"%s\" %s", ytDlpPath, downloadDir, youtubeUrl);
+
             executeCommand(downloadCommand);
-            // Extract audio
-            executeCommand(extractCommand);
+
+            filename = executeCommand(printCommand);
+
+            if (filename != null) {
+                String ext = filename.substring(filename.lastIndexOf("."));
+                filename = filename.replace(ext, ".mp3");
+            }
+
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        return " ";
+        log.info("filename: {}" ,filename);
+        return filename;
     }
 
-    private void executeCommand(String command) throws IOException, InterruptedException {
+    private String executeCommand(String command) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime().exec(command);
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
+        int i = 0;
+        String filename = null;
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            filename = line;
         }
         process.waitFor();
+
+        return filename;
     }
 }
